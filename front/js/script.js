607140bdcf578ffd213b5a2eedaa2c8e08b88d9f
Nicolas Demangeat <demangeatn@gmail.com>
2022-11-01 14:16:58 +0100

    ajout JS page index

--- front/js/script.js
+++ front/js/script.js
@@ -1,20 +1,36 @@
-/* main fonction */
-const fetchKanapData = async function(){
-    return await fetch("http://localhost:3000/api/products/")
-    .then(response => response.json())
-    .then(data => {return data});
-}
-console.log("test : " + fetchKanapData().then(datas => {
-    for (kanap in datas[0]){
-        console.log("je saiss pas " + kanap["description"])
-    };
-}));
 
-const fetchData = async function () {
+
+/* main fonction */
+const fetchData = async function(){
     return await fetch("http://localhost:3000/api/products/")
     .then(response => response.json())
-    .then(data => {return data})
     .catch(e => console.log("il y a une erreur de type : " + e));
 }
 
-console.log("deuxieme kanapData" + fetchData());
+const main = async function(){
+    const data = await fetchData();
+
+    const section = document.getElementById('items');
+    
+    for (kanap of data){
+        let a = document.createElement("a");
+        a.setAttribute('href', '../html/product.html?id=' + kanap._id);
+        let article = document.createElement('article');
+        let img = document.createElement('img');
+        img.setAttribute('src', kanap.imageUrl);
+        img.setAttribute('alt', kanap.altTxt);
+        let h3 = document.createElement('h3');
+        h3.setAttribute('class', 'productName');
+        h3.textContent = kanap.name;
+        let p = document.createElement('p');
+        p.setAttribute('class', 'productDescription');
+        p.textContent = kanap.description;
+        article.appendChild(img);
+        article.appendChild(h3);
+        article.appendChild(p);
+        a.appendChild(article);
+        section.appendChild(a);
+    }
+}
+
+main();
